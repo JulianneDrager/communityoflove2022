152d0581ee919deb0ba848a0b60c8fa93a7aea67
clarefausty <clarefausty@gmail.com>
2023-09-30 11:18:35 +0100

    WIP

---
+++ src/Components/dataDummy.js
@@ -0,0 +1,19 @@
+const dataDummy=
+    [
+    {
+        id: 1,
+        quote: "Boost your product and service's credibility by adding testimonials from your clients. People love recommendations so feedback from others who'ever tried it is invaluable",
+        name: "Hannah Morales"
+    },
+    {
+        id: 2,
+        quote: "Boost your product and service's credibility by adding testimonials from your clients. People love recommendations so feedback from others who'ever tried it is invaluable",
+        name: "Teddy Yuhuri"
+    },
+    {
+        id: 2,
+        quote: "Boost your product and service's credibility by adding testimonials from your clients. People love recommendations so feedback from others who'ever tried it is invaluable",
+        name: "Lorna Alvarado"
+    },
+]
+export default dataDummy;
