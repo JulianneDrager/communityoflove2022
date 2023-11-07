2ee93b77521c2caad99d8a81fe51af6d65dbc27e
Jacob G <jrgrant560@gmail.com>
2023-11-07 13:30:50 -0500

    built out base of content component; need to add styling and iteration connections

--- src/Components/FamilyServices/services-components/services-content/services-content.js
+++ src/Components/FamilyServices/services-components/services-content/services-content.js
@@ -6,12 +6,24 @@ import ContentStyles from "./css/services-content.module.css";
 const ServicesContent = () => {
 
     const mainContainer = ContentStyles.mainContainer;
+    const iconImage = ContentStyles.iconImage;
+    const h3Style = ContentStyles.h3Style;
+    const h4Style = ContentStyles.h4Style;
+    const article = ContentStyles.article;
+    const priceRow = ContentStyles.priceRow;
 
     return (
         <>
-            <div className={mainContainer}>
-                Services Content is working!
-            </div>
+            <main className={mainContainer}>
+                <div className={iconImage}></div>
+                <h3 className={h3Style}>question here</h3>
+                <h4 className={h4Style}>subtitle here</h4>
+                <article className={article}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</article>
+                <row className={priceRow}>
+                    <p>$ 5.00/month</p>
+                    <p>$ 50.00/year</p>
+                </row>
+            </main>
         </>
     )
 }
